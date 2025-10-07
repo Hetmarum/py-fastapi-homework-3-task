10719651919932f4ef0d604ed283439f876d8c6c
Hetmarum <kpxmedet@gmail.com>
2025-10-07 17:18:42 +0300

    flake8 fix

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -258,6 +258,7 @@ async def login_user(
         await db.rollback()
         raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
 
+
 @router.post(
     "/refresh/",
     response_model=TokenRefreshResponseSchema,
